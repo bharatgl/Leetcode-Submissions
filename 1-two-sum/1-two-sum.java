b1121dc0e887b25c90c04bd6fdaa4b9c83703162
Bharat goyal <56772219+bharatgl@users.noreply.github.com>
2022-05-30 03:19:43 +0530

    Time: 4 ms (69.00%), Space: 45.7 MB (34.20%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,16 +1,29 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] ans = new int[2]; 
-       for(int i=0; i<nums.length; i++){
-           for(int j=i+1 ; j<nums.length; j++){
-               if(nums[i] + nums[j] == target){
-                   ans[0] =i;
-                   ans[1] = j;
-                   break;
-               }
-           }
+//         int[] ans = new int[2]; 
+//        for(int i=0; i<nums.length; i++){
+//            for(int j=i+1 ; j<nums.length; j++){
+//                if(nums[i] + nums[j] == target){
+//                    ans[0] =i;
+//                    ans[1] = j;
+//                    break;
+//                }
+//            }
           
-       }
-        return ans;
+//        }
+//         return ans;
+
+
+         HashMap<Integer,Integer> hm = new HashMap<Integer,Integer>();
+        for(int i = 0;i<nums.length;i++){
+            int complement = target - nums[i];
+            if(hm.containsKey(complement)){
+                return new int []{hm.get(complement),i};
+            }else{
+                hm.put(nums[i],i);
+            }
+        }
+        return new int []{};
+        
     }
 }
