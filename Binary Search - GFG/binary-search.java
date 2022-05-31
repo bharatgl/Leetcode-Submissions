dd355050e8d3267bac9610e932bdbfc2a93f9f2f
Bharat goyal <56772219+bharatgl@users.noreply.github.com>
2022-05-31 19:06:17 +0530

    Added solution - LeetHub

---
+++ Binary Search - GFG/binary-search.java
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+  public class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            int key = sc.nextInt();
+            Solution g = new Solution();
+            System.out.println(g.binarysearch(arr, n, key));
+            T--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int binarysearch(int arr[], int n, int k) {
+         int l = 0;
+       int r = n-1;
+       
+       while(l<=r)
+       {
+           int mid = (l+r)/2;
+       if(k<arr[mid])
+       {
+           r = mid -1;
+       }
+       else if(k == arr[mid])
+       {
+           return mid;
+       }
+       else{
+           
+           l = mid + 1;
+           
+           }
+       }
+       return -1;
+       
+   }
+}
