c857ac52bd4009f9858e2b7e89e010882463b201
Bharat goyal <56772219+bharatgl@users.noreply.github.com>
2022-06-05 00:00:00 +0530

    Time: 78 ms (54.56%), Space: 41.9 MB (70.55%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function(nums) {
+    var count = 0
+    for(var i=0; i<nums.length; i++){
+        
+        for(var j=i+1; j<nums.length; j++){
+            if(nums[i] == nums[j]){
+                count++
+                
+            }
+        }
+    }
+    return count
+    
+};
