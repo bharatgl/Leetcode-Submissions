9127bd89f2858498e17ce59c6acacb86b3b6415c
Bharat goyal <56772219+bharatgl@users.noreply.github.com>
2022-06-10 02:02:29 +0530

    Time: 1 ms (84.62%), Space: 42.4 MB (28.32%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,59 +1,48 @@
-// class Solution {
-//     public String interpret(String command) {
-//         //Goal Parser ----> command ----> "G", "()" and/or "(al)"
-//         // "G" ------> "G", "()"
-//         // "o", and "(al)" -----> "al"
-//         // G()al
-//      StringBuilder sb = new StringBuilder();
-//         for(int i = 0;i<command.length();i++){
-//             if(command.charAt(i)=="G"){
-//                 sb.append("G");
-//             }else if(command.charAt(i)=="("){
-//                 if(command.charAt(i+1)==")"){
-//                      sb.append("o");
-//                       i++;
-//                 }
-//             }else if(command.charAt(i)=="A"){
-//                  sb.append("al");
-//                   i = i+3;
-//             }
-        
-//         return sb.toString();
-//        }
-//     }
-// }
+
+
             
-//     //     if(string.includes("()")){
-//     //     string = string.replaceAll("()", "o")
-//     //   }
-//     //     if(string.includes("(al)")){
-//     //     string = string.replaceAll("(al)", "al")
-//     //  }
-//     // return string
-        
-        class Solution {
+class Solution {
     public String interpret(String command) {
-        String result = "" ;
-        
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < command.length(); i++) {
-            if (command.charAt(i) == 'G') {
-                result += 'G' ;
-            }
+            if (command.charAt(i) == 'G') sb.append("G");
             else if (command.charAt(i) == '(') {
                 if (command.charAt(i+1) == ')') {
-                    result += 'o' ;
-                    i++ ;
-                }
-                else {
-                    result += "al" ;
-                    i += 3 ;
+                    sb.append("o");
+                    i++;
+                } else if (command.charAt(i+1) == 'a') {
+                    sb.append("al");
+                    i += 3;
                 }
             }
         }
-        
-        return result ;
+        return sb.toString();
     }
 }
+        
+//         class Solution {
+//     public String interpret(String command) {
+//         String result = "" ;
+        
+//         for (int i = 0; i < command.length(); i++) {
+//             if (command.charAt(i) == 'G') {
+//                 result += 'G' ;
+//             }
+//             else if (command.charAt(i) == '(') {
+//                 if (command.charAt(i+1) == ')') {
+//                     result += 'o' ;
+//                     i++ ;
+//                 }
+//                 else {
+//                     result += "al" ;
+//                     i += 3 ;
+//                 }
+//             }
+//         }
+        
+//         return result ;
+//     }
+// }
         
         
     
