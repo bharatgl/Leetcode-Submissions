fcd015673cd933a48f26221d184ab7db062c46e5
Bharat goyal <56772219+bharatgl@users.noreply.github.com>
2022-09-15 23:43:36 +0530

    Time: 84 ms (5.15%), Space: 51.7 MB (5.33%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+        String str=String.valueOf(x);
+            String rev = "";
+ 
+        // Initializing a new boolean variable for the
+        // answer
+        boolean ans = false;
+ 
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+ 
+        // Checking if both the strings are equal
+        if (str.equals(rev)) {
+            ans = true;
+        }
+        return ans;
+    }
+    }
